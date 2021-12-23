9ce7889307e0652803b8e70e76d8ea904f06bad9
jub3907 <side1923@naver.com>
2021-12-23 22:01:14 +0900

    Spring 강의 12일차
    @configuration과 싱글톤

--- core/core/src/main/java/hello/core/AppConfig.java
+++ core/core/src/main/java/hello/core/AppConfig.java
@@ -16,11 +16,13 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
